37348228ac3c5697c1f2a33562760f978aade8ef
SmillaLerche <smillalerche1@gmail.com>
2023-01-06 15:05:26 +0100

    hejjj

---
+++ portfolio2/script.js
@@ -0,0 +1,67 @@
+window.addEventListener("load", sidenVises);
+
+function sidenVises(){
+    hideAllScreens();
+    
+document.querySelector("#tema_2_knap").addEventListener("mousedown", tema2);
+document.querySelector("#tema_3_knap").addEventListener("mousedown", tema3);
+document.querySelector("#tema_4_knap").addEventListener("mousedown", tema4);
+document.querySelector("#tema_5_knap").addEventListener("mousedown", tema5);
+document.querySelector("#tema_6_knap").addEventListener("mousedown", tema6);
+}
+
+function hideAllScreens() {
+    document.querySelector("#tema_2_img").classList.add("hide");
+    document.querySelector("#tema_22_img").classList.add("hide");
+
+    document.querySelector("#tema_3_img").classList.add("hide");
+
+    document.querySelector("#tema_4_img").classList.add("hide");
+
+    document.querySelector("#tema_5_img").classList.add("hide");
+    document.querySelector("#tema_55_img").classList.add("hide");
+
+ document.querySelector("#tema_6_img").classList.add("hide");
+
+}
+
+function tema2(){
+    hideAllScreens();
+    document.querySelector("#tema_2_img").classList.remove("hide");
+    document.querySelector("#tema_22_img").classList.remove("hide");
+    document.querySelector("#tema_2_knap").addEventListener("click", tema2);
+    
+    
+}
+
+function tema3(){
+    hideAllScreens();
+    document.querySelector("#tema_3_img").classList.remove("hide");
+    document.querySelector("#tema_3_knap").addEventListener("click", tema3);
+}
+
+
+function tema4(){
+    hideAllScreens();
+    document.querySelector("#tema_4_img").classList.remove("hide");
+    document.querySelector("#tema_4_knap").addEventListener("click", tema4);
+}
+
+function tema5(){
+    hideAllScreens();
+    document.querySelector("#tema_5_img").classList.remove("hide");
+    document.querySelector("#tema_55_img").classList.remove("hide");
+    document.querySelector("#tema_5_knap").addEventListener("click", tema5);
+}
+
+function tema6(){
+    hideAllScreens();
+    document.querySelector("#tema_6_img").classList.remove("hide")
+    document.querySelector("#tema_6_knap").addEventListener("click", tema6)
+}
+
+
+
+
+
+
