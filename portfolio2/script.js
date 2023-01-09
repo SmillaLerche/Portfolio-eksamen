5fc0a4d412087c204cd8eab2a995de71f05eb66d
SmillaLerche <smillalerche1@gmail.com>
2023-01-09 16:54:22 +0100

    links sat ind

--- portfolio2/script.js
+++ portfolio2/script.js
@@ -23,6 +23,16 @@ function hideAllScreens() {
 
  document.querySelector("#tema_6_img").classList.add("hide");
 
+ document.querySelector("#website_link").classList.add("hide")
+ document.querySelector("#emnesite_link").classList.add("hide")
+
+ document.querySelector("#løsning_link").classList.add("hide")
+
+ document.querySelector("#animation_link").classList.add("hide")
+
+ document.querySelector("#pilotsite_link").classList.add("hide")
+ document.querySelector("#redesign_link").classList.add("hide")
+
 }
 
 function tema2(){
@@ -30,6 +40,9 @@ function tema2(){
     document.querySelector("#tema_2_img").classList.remove("hide");
     document.querySelector("#tema_22_img").classList.remove("hide");
     document.querySelector("#tema_2_knap").addEventListener("click", tema2);
+    document.querySelector("#website_link").classList.remove("hide")
+    document.querySelector("#emnesite_link").classList.remove("hide")
+    
     
     
 }
@@ -38,6 +51,7 @@ function tema3(){
     hideAllScreens();
     document.querySelector("#tema_3_img").classList.remove("hide");
     document.querySelector("#tema_3_knap").addEventListener("click", tema3);
+    document.querySelector("#løsning_link").classList.remove("hide")
 }
 
 
@@ -45,6 +59,7 @@ function tema4(){
     hideAllScreens();
     document.querySelector("#tema_4_img").classList.remove("hide");
     document.querySelector("#tema_4_knap").addEventListener("click", tema4);
+    document.querySelector("#animation_link").classList.remove("hide")
 }
 
 function tema5(){
@@ -52,6 +67,8 @@ function tema5(){
     document.querySelector("#tema_5_img").classList.remove("hide");
     document.querySelector("#tema_55_img").classList.remove("hide");
     document.querySelector("#tema_5_knap").addEventListener("click", tema5);
+    document.querySelector("#pilotsite_link").classList.remove("hide")
+ document.querySelector("#redesign_link").classList.remove("hide")
 }
 
 function tema6(){
